637bf5f09b4bd36b0593307b3b3461f61a30459c
YOUJIN LEE <jess.lee0124@gmail.com>
2016-09-25 18:24:19 -0400

    update comments

--- js/app.js
+++ js/app.js
@@ -10,8 +10,6 @@ var star1Removed = false;
 
 // Enemies our player must avoid
 var Enemy = function(x, y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
     this.x = x;
     this.y = y;
     this.sprite = 'images/enemy-bug.png';
@@ -21,7 +19,7 @@ var Enemy = function(x, y) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+    // Multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
     if (this.x < width) {
@@ -73,7 +71,7 @@ Enemy.prototype.checkCollisions = function(player) {
     } 
 };
 
-// Now write your own player class
+// Create Player class
 var Player = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -82,18 +80,19 @@ var Player = function() {
     this.sprite = 'images/char-boy.png';
 };
 
-// This class requires an update(), render() and
-// a handleInput() method.
+// Update the player
 Player.prototype.update = function() {
     if (gameOver === true) {
         this.reset();
     }
 }
 
+// Renders the player image on canvas
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+// Moves the players after detecting the keyboard input(up,down,right,left)
 Player.prototype.handleInput = function(key) {
     if (key === 'up') {
         if (this.y > 0 && this.y <= height) {
@@ -157,15 +156,12 @@ Player.prototype.reset = function(score, life) {
     }
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Instantiate objects
 var allEnemies = [new Enemy(200, 227), new Enemy(100, 144), new Enemy(100, 61)];
 var player = new Player();
 alert("You have 3 chances to get to water! Press enter to start the game...");
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to handleInput()
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
